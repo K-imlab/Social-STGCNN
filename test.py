475429d0d3fb12228a17dd4eebeacbda726ff141
H.J. Kim <48542752+K-imlab@users.noreply.github.com>
2021-05-25 00:15:31 +0900

    210525 change device cpu or gpu

--- test.py
+++ test.py
@@ -14,19 +14,22 @@ from metrics import *
 from model import social_stgcnn
 import copy
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
 def test(KSTEPS=20):
     global loader_test,model
     model.eval()
     ade_bigls = []
     fde_bigls = []
     raw_data_dict = {}
-    step =0 
+    step = 0
     for batch in loader_test: 
-        step+=1
+        step += 1
         #Get data
-        batch = [tensor.cuda() for tensor in batch]
+        batch = [tensor.to(device) for tensor in batch]
         obs_traj, pred_traj_gt, obs_traj_rel, pred_traj_gt_rel, non_linear_ped,\
-         loss_mask,V_obs,A_obs,V_tr,A_tr = batch
+         loss_mask, V_obs, A_obs, V_tr, A_tr = batch
 
 
         num_of_objs = obs_traj_rel.shape[1]
@@ -36,7 +39,7 @@ def test(KSTEPS=20):
         #V_obs_tmp = batch,feat,seq,node
         V_obs_tmp =V_obs.permute(0,3,1,2)
 
-        V_pred,_ = model(V_obs_tmp,A_obs.squeeze())
+        V_pred, _ = model(V_obs_tmp,A_obs.squeeze())
         # print(V_pred.shape)
         # torch.Size([1, 5, 12, 2])
         # torch.Size([12, 2, 5])
@@ -49,24 +52,24 @@ def test(KSTEPS=20):
         A_tr = A_tr.squeeze()
         V_pred = V_pred.squeeze()
         num_of_objs = obs_traj_rel.shape[1]
-        V_pred,V_tr =  V_pred[:,:num_of_objs,:],V_tr[:,:num_of_objs,:]
-        #print(V_pred.shape)
+        V_pred, V_tr = V_pred[:, :num_of_objs, :], V_tr[:, :num_of_objs, :]
+        # print(V_pred.shape)
 
-        #For now I have my bi-variate parameters 
-        #normx =  V_pred[:,:,0:1]
-        #normy =  V_pred[:,:,1:2]
-        sx = torch.exp(V_pred[:,:,2]) #sx
-        sy = torch.exp(V_pred[:,:,3]) #sy
-        corr = torch.tanh(V_pred[:,:,4]) #corr
+        # For now I have my bi-variate parameters
+        # normx =  V_pred[:,:,0:1]
+        # normy =  V_pred[:,:,1:2]
+        sx = torch.exp(V_pred[:,:,2])  # sx
+        sy = torch.exp(V_pred[:,:,3])  # sy
+        corr = torch.tanh(V_pred[:,:,4])  # corr
         
-        cov = torch.zeros(V_pred.shape[0],V_pred.shape[1],2,2).cuda()
-        cov[:,:,0,0]= sx*sx
-        cov[:,:,0,1]= corr*sx*sy
-        cov[:,:,1,0]= corr*sx*sy
-        cov[:,:,1,1]= sy*sy
-        mean = V_pred[:,:,0:2]
+        cov = torch.zeros(V_pred.shape[0],V_pred.shape[1],2,2).to(device)
+        cov[:, :, 0, 0] = sx*sx
+        cov[:, :, 0, 1] = corr*sx*sy
+        cov[:, :, 1, 0] = corr*sx*sy
+        cov[:, :, 1, 1] = sy*sy
+        mean = V_pred[:, :, 0:2]
         
-        mvnormal = torchdist.MultivariateNormal(mean,cov)
+        mvnormal = torchdist.MultivariateNormal(mean, cov)
 
 
         ### Rel to abs 
@@ -134,10 +137,7 @@ print('Number of samples:',KSTEPS)
 print("*"*50)
 
 
-
-
 for feta in range(len(paths)):
-    print(feta)
     ade_ls = [] 
     fde_ls = []
     path = paths[feta]
@@ -183,8 +183,8 @@ for feta in range(len(paths)):
         #Defining the model 
         model = social_stgcnn(n_stgcnn =args.n_stgcnn,n_txpcnn=args.n_txpcnn,
         output_feat=args.output_size,seq_len=args.obs_seq_len,
-        kernel_size=args.kernel_size,pred_seq_len=args.pred_seq_len).cuda()
-        model.load_state_dict(torch.load(model_path))
+        kernel_size=args.kernel_size,pred_seq_len=args.pred_seq_len).to(device)
+        model.load_state_dict(torch.load(model_path, map_location=device))
 
 
         ade_ =999999
